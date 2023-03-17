ae26459e687725797bbfa4e985a4e16cc8e48023
DrizzyJ56 <blevins.j921@gmail.com>
2023-03-17 09:54:29 -0400

    trying to add to db

--- db/client.js
+++ db/client.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg");
 
 const connectionString =
-  process.env.DATABASE_URL || "https://localhost:5432/fitness-dev";
+  process.env.DATABASE_URL;
 
 const client = new Pool({
   connectionString,
