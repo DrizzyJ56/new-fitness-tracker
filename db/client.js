a027a38b236c220ca7f161a27a7100144f0d3b88
DrizzyJ56 <blevins.j921@gmail.com>
2023-03-21 10:06:05 -0400

    trying to fix webservice v3

--- db/client.js
+++ db/client.js
@@ -1,15 +1,26 @@
-const { Pool } = require("pg");
+// const { Pool } = require("pg");
 
-const connectionString =
-  process.env.DATABASE_URL;
+// const connectionString =
+//   process.env.DATABASE_URL;
 
-const client = new Pool({
+// const client = new Pool({
+//   connectionString,
+//   ssl:
+//     process.env.NODE_ENV === "production"
+//       ? { rejectUnauthorized: false }
+//       : undefined,
+// //   idleTimeoutMillis: 30000    
+// });
+
+// module.exports = client;
+
+const { Client } = require('pg');
+
+const connectionString = process.env.DATABASE_URL || 'https://localhost:5432/fitness-dev';
+
+const client = new Client({
   connectionString,
-  ssl:
-    process.env.NODE_ENV === "production"
-      ? { rejectUnauthorized: false }
-      : undefined,
-  idleTimeoutMillis: 30000    
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
 });
 
 module.exports = client;
